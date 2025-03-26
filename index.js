ecea62ab62f2480601656427dfe9e73fb6ae2614
Sushmita Kumari Mahato <140884185+Sushmita2k04@users.noreply.github.com>
2025-03-26 11:36:43 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('HI AWS')
 })
 
 app.get('/courses', function (req, res) {
